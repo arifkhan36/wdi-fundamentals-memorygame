275ba902fcf505921798793eba6fb0c62de54a89
md khan <arifk36@yahoo.com>
2017-07-11 19:13:17 -0400

    added if else statement into js file

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ var cardTwo =['king'];
 cardsInPlay.push('cardOne');
 cardsInPlay.push('cardTwo');
 
-console.log("user flipped" +" "+"cardOne");
-console.log("user flipped" +" "+"cardTwo");
+console.log("user flipped queen");
+console.log("user flipped king");
+
+
+ if (cardsInPlay.length===2) {
+    if (cardsInPlay[0]===cardsInPlay[1]){
+     alert("you found a match");
+    }else{
+    alert("sorry, try again!");
+   }
+  }
+
+
+
+
+
