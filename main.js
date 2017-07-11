9001d368f6935aa4c87fdc735173ebff3739491e
md khan <arifk36@yahoo.com>
2017-07-11 16:20:39 -0400

    added some javascript array

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var cardOne ="queen";
-var cardTwo ="king";
-var cardThree ="queen";
-var cardFour ="king";
-console.log("user flipped" +" "+cardOne);
-console.log("user flipped" +" "+cardTwo);
-console.log("user flipped" +" " +cardThree);
+
+var cards = ['queen','queen','king','king'];
+var cardsInPlay=[];
+var cardOne =['queen'];
+var cardTwo =['king'];
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+console.log("user flipped" +" "+"cardOne");
+console.log("user flipped" +" "+"cardTwo");
